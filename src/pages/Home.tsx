7f950dbba66d5f5a390f4ab26a81a85f122ce702
sabarinathan178 <sabarinathanbabu178@gmail.com>
2025-01-24 22:42:12 +0530

    image1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
-import { useSpring, animated } from "react-spring";
+import { animated } from "react-spring";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
